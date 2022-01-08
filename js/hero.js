38db12a1d6dcea0346372af984231a293d9615da
glassbarricades <glassbarricades1@list.ru>
2022-01-08 19:45:29 +0300

    hero page dev

--- js/hero.js
+++ js/hero.js
@@ -10,61 +10,84 @@ window.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
+    const arrSlice = (start, count, data) => {
+        let arr = data.slice(start, count);
+        return arr;
+    };
+
     // getResource('https://rickandmortyapi.com/api/character/')
     //     .then(data => createCharacter(data));
 
     Promise.all([
-        getResource('https://rickandmortyapi.com/api/character/'),
-        getResource('https://rickandmortyapi.com/api/character/?page=2'),
-        getResource('https://rickandmortyapi.com/api/character/?page=3'),
-        getResource('https://rickandmortyapi.com/api/character/?page=4'),
-        getResource('https://rickandmortyapi.com/api/character/?page=5'),
-        getResource('https://rickandmortyapi.com/api/character/?page=6'),
-        getResource('https://rickandmortyapi.com/api/character/?page=7'),
-        getResource('https://rickandmortyapi.com/api/character/?page=8'),
-        getResource('https://rickandmortyapi.com/api/character/?page=9'),
-        getResource('https://rickandmortyapi.com/api/character/?page=10'),
-        getResource('https://rickandmortyapi.com/api/character/?page=11'),
-        getResource('https://rickandmortyapi.com/api/character/?page=12'),
-        getResource('https://rickandmortyapi.com/api/character/?page=13'),
-        getResource('https://rickandmortyapi.com/api/character/?page=14'),
-        getResource('https://rickandmortyapi.com/api/character/?page=15'),
-        getResource('https://rickandmortyapi.com/api/character/?page=16'),
-        getResource('https://rickandmortyapi.com/api/character/?page=17'),
-        getResource('https://rickandmortyapi.com/api/character/?page=18'),
-        getResource('https://rickandmortyapi.com/api/character/?page=19'),
-        getResource('https://rickandmortyapi.com/api/character/?page=20'),
-        getResource('https://rickandmortyapi.com/api/character/?page=21'),
-        getResource('https://rickandmortyapi.com/api/character/?page=22'),
-        getResource('https://rickandmortyapi.com/api/character/?page=23'),
+            getResource('https://rickandmortyapi.com/api/character/'),
+            getResource('https://rickandmortyapi.com/api/character/?page=2'),
+            getResource('https://rickandmortyapi.com/api/character/?page=3'),
+            getResource('https://rickandmortyapi.com/api/character/?page=4'),
+            getResource('https://rickandmortyapi.com/api/character/?page=5'),
+            getResource('https://rickandmortyapi.com/api/character/?page=6'),
+            getResource('https://rickandmortyapi.com/api/character/?page=7'),
+            getResource('https://rickandmortyapi.com/api/character/?page=8'),
+            getResource('https://rickandmortyapi.com/api/character/?page=9'),
+            getResource('https://rickandmortyapi.com/api/character/?page=10'),
+            getResource('https://rickandmortyapi.com/api/character/?page=11'),
+            getResource('https://rickandmortyapi.com/api/character/?page=12'),
+            getResource('https://rickandmortyapi.com/api/character/?page=13'),
+            getResource('https://rickandmortyapi.com/api/character/?page=14'),
+            getResource('https://rickandmortyapi.com/api/character/?page=15'),
+            getResource('https://rickandmortyapi.com/api/character/?page=16'),
+            getResource('https://rickandmortyapi.com/api/character/?page=17'),
+            getResource('https://rickandmortyapi.com/api/character/?page=18'),
+            getResource('https://rickandmortyapi.com/api/character/?page=19'),
+            getResource('https://rickandmortyapi.com/api/character/?page=20'),
+            getResource('https://rickandmortyapi.com/api/character/?page=21'),
+            getResource('https://rickandmortyapi.com/api/character/?page=22'),
+            getResource('https://rickandmortyapi.com/api/character/?page=23'),
 
-    ])
-    .then(data2 => {
-        // data2.forEach((item, i) => {
-        //     let arr = data2[i].results;
-        //     let sum = arr.concat(arr[i+1])
-        //     console.log(sum);
-        // })
-         let arr1 = data2[0].results;
-         let arr2 = data2[1].results;
-         let arr3 = data2[2].results;
-         let arr6 = data2[3].results;
-         let arr8 = data2[4].results;
-         let arr4 = arr1.concat(arr2);
-         let arr5 = arr4.concat(arr3);
-         let arr7 = arr5.concat(arr6);
-         let arr9 = arr7.concat(arr8);
-         console.log(arr9);
+        ])
+        .then(data2 => {
+            let arr = [];
 
-    })
-    .then((data) => createCharacter(data));
+            data2.forEach((item, i) => {
+                arr.push(item.results);
+            });
+            return arr;
+
+        })
+        .then((data3) => {
+            let res = data3.reduce((sum, current) => {
+                let i = sum.concat(current);
+                return i;
+            });
+            return res;
+        })
+        .then((data) => createCharacter(data));
 
 
     function parseCharacter(data) {
-        data.forEach((item, i) => {
-            const element = document.createElement('p');
+        // let dataSlice = data.slice(0, 20);
+        let dataSlice = arrSlice(0, 20, data);
+        dataSlice.forEach((item, i) => {
+            const element = document.createElement('div');
+            element.classList.add('col-md-3')
             element.innerHTML = `
-            <p>${item.id}) Герой: ${item.name}, Статус: ${item.status}, Пол: ${item.gender}</p>
+            <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src=${item.image} class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${item.name}</h5>
+                        <p class="card-text">
+                        Статус: ${item.status}<br>
+                        Раса: ${item.species}<br>
+                        Пол: ${item.gender}<br>
+                        </p>
+                        <p class="card-text"><small class="text-muted"></small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
         `;
 
             document.querySelector('.app2').append(element);
